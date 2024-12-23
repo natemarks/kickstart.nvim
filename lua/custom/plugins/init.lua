89a676458dd7c0a97d08840b6c265ddacb6f6b55
Nate Marks <nmarks@imprivata.com>
2024-12-23 06:48:03 -0500

    parametetrized pyttest snippet

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -39,14 +39,14 @@ local conds = require 'luasnip.extras.conditions'
 local conds_expand = require 'luasnip.extras.conditions.expand'
 
 ls.add_snippets('python', {
-  s('pyt', {
+  s('ppyt', {
     t {
       '@pytest.mark.unit',
       '@pytest.mark.parametrize(',
       '    "',
     },
-    i(1, "config_dir"),
-    t{
+    i(1, 'config_dir'),
+    t {
       '",',
       '    [',
       '        pytest.param("config/sandbox/rel1977_1", id="sandbox_actual"),',
@@ -54,7 +54,30 @@ ls.add_snippets('python', {
       ')',
       'def ',
     },
-    i(2, "test_app_vpc_stack"),
+    i(2, 'test_app_vpc_stack'),
+    t {
+      '(request, config_dir, update_golden):',
+      '    """test app_vpc_stack"""',
+      '',
+      '    case = Case(request)',
+      '    s_input = AppVpcInput.from_config_directory(',
+      '        Case.abs_path_from_project_root(config_dir)',
+      '    )',
+      '',
+      '    app = App()',
+      '',
+      '    stk = AppVpcStack(',
+      '        scope=app,',
+      '        construct_id=f"{{s_input.app_vpc.construct_id_prefix}}Stack",',
+      '        cdk_env=Environment(),',
+      '        s_input=s_input,',
+      '    )',
+      '    template = assertions.Template.from_stack(stk)',
+      '    if update_golden:',
+      '        case.update_expected(template.to_json))',
+      '',
+      '    template.template_matches(case.expected())',
+    },
   }),
 })
 return M
