a1a965deb01808a783096816972923d6008f51ff
Nate Marks <nmarks@imprivata.com>
2024-12-25 04:55:09 -0500

    neotest runs. no keymaps yet

--- init.lua
+++ init.lua
@@ -963,7 +963,23 @@ require('lazy').setup({
       'nvim-lua/plenary.nvim',
       'antoinemadec/FixCursorHold.nvim',
       'nvim-treesitter/nvim-treesitter',
+      'nvim-neotest/neotest-python',
+      'nvim-neotest/neotest-plenary',
+      'nvim-neotest/neotest-vim-test',
     },
+    config = function()
+      require('neotest').setup {
+        adapters = {
+          require 'neotest-python' {
+            dap = { justMyCode = false },
+          },
+          require 'neotest-plenary',
+          require 'neotest-vim-test' {
+            ignore_file_types = { 'python', 'vim', 'lua' },
+          },
+        },
+      }
+    end,
   },
   {
     'nvim-neotest/neotest-python',
