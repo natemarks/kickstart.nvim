6883c9ddb1fcf604ef43515b6eed67e4c07e8729
Nate Marks <npmarks@gmail.com>
2026-03-03 09:31:28 -0500

    mini plugins

--- init.lua
+++ init.lua
@@ -424,55 +424,7 @@ require('lazy').setup({
   -- Highlight todo, notes, etc in comments
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
   require 'kickstart.plugins.harpoon',
-  { -- Collection of various small independent plugins/modules
-    'echasnovski/mini.nvim',
-    config = function()
-      -- Better Around/Inside textobjects
-      --
-      -- Examples:
-      --  - va)  - [V]isually select [A]round [)]paren
-      --  - yinq - [Y]ank [I]nside [N]ext [Q]uote
-      --  - ci'  - [C]hange [I]nside [']quote
-      require('mini.ai').setup { n_lines = 500 }
-
-      -- Add/delete/replace surroundings (brackets, quotes, etc.)
-      --
-      -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
-      -- - sd'   - [S]urround [D]elete [']quotes
-      -- - sr)'  - [S]urround [R]eplace [)] [']
-      require('mini.surround').setup {
-        mappings = {
-          add = 'gsa',
-          delete = 'gsd',
-          replace = 'gsr',
-          find = 'gsf',
-          find_left = 'gsF',
-          highlight = 'gsh',
-          update_n_lines = 'gsn',
-          suffix_next = '',
-          suffix_last = '',
-        },
-      }
-
-      -- Simple and easy statusline.
-      --  You could remove this setup call if you don't like it,
-      --  and try some other statusline plugin
-      local statusline = require 'mini.statusline'
-      -- set use_icons to true if you have a Nerd Font
-      statusline.setup { use_icons = vim.g.have_nerd_font }
-
-      -- You can configure sections in the statusline by overriding their
-      -- default behavior. For example, here we set the section for
-      -- cursor location to LINE:COLUMN
-      ---@diagnostic disable-next-line: duplicate-set-field
-      statusline.section_location = function()
-        return '%2l:%-2v'
-      end
-
-      -- ... and there is more!
-      --  Check out: https://github.com/echasnovski/mini.nvim
-    end,
-  },
+  require 'kickstart.plugins.mini',
   require 'kickstart.plugins.treesitter',
   {
     'nvim-neotest/neotest',
---
+++ lua/kickstart/plugins/mini/init.lua
@@ -0,0 +1,6 @@
+return {
+  'echasnovski/mini.nvim',
+  config = function()
+    require('kickstart.plugins.mini.config').setup()
+  end,
+}
