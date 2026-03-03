d8ac211565a656cfa8463fd19a42f1bc5a91ab13
Nate Marks <npmarks@gmail.com>
2026-03-03 08:12:49 -0500

    fix deprecated

--- lua/kickstart/lsp/core.lua
+++ lua/kickstart/lsp/core.lua
@@ -25,10 +25,10 @@ function M.setup_on_attach()
       map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
 
       local client = vim.lsp.get_client_by_id(event.data.client_id)
-      local has_inlay_hints = client and client.supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint)
+      local has_inlay_hints = client and client:supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint)
       require('kickstart.lsp.which_key').add(event.buf, has_inlay_hints)
 
-      if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight) then
+      if client and client:supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight) then
         local highlight_augroup = vim.api.nvim_create_augroup('kickstart-lsp-highlight', { clear = false })
         vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
           buffer = event.buf,
