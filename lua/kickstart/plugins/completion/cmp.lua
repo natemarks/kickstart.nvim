e32427298c860bca379080a636f7edd517bcaa4e
Nate Marks <npmarks@gmail.com>
2026-03-03 14:23:44 -0500

    completion keys

--- lua/kickstart/plugins/completion/cmp.lua
+++ lua/kickstart/plugins/completion/cmp.lua
@@ -4,6 +4,20 @@ function M.setup()
   local cmp = require 'cmp'
   local luasnip = require 'luasnip'
 
+  local function copilot_visible()
+    return vim.fn.exists '*copilot#Visible' == 1 and vim.fn['copilot#Visible']() == 1
+  end
+
+  local function copilot_accept()
+    local ok, accepted = pcall(vim.fn['copilot#Accept'], '')
+    if not ok or not accepted then
+      return false
+    end
+
+    vim.api.nvim_feedkeys(accepted, 'i', true)
+    return true
+  end
+
   cmp.setup {
     snippet = {
       expand = function(args)
@@ -16,7 +30,18 @@ function M.setup()
       ['<C-p>'] = cmp.mapping.select_prev_item(),
       ['<C-b>'] = cmp.mapping.scroll_docs(-4),
       ['<C-f>'] = cmp.mapping.scroll_docs(4),
-      ['<C-y>'] = cmp.mapping.confirm { select = true },
+      ['<C-y>'] = cmp.mapping(function(fallback)
+        if copilot_visible() and copilot_accept() then
+          return
+        end
+
+        if cmp.visible() then
+          cmp.confirm { select = true }
+          return
+        end
+
+        fallback()
+      end, { 'i' }),
       ['<CR>'] = cmp.mapping.confirm { select = true },
       ['<Tab>'] = cmp.mapping.select_next_item(),
       ['<S-Tab>'] = cmp.mapping.select_prev_item(),
---
+++ lua/kickstart/plugins/copilot/init.lua
@@ -0,0 +1,17 @@
+return {
+  'github/copilot.vim',
+  event = 'InsertEnter',
+  init = function()
+    vim.g.copilot_no_tab_map = true
+    vim.g.copilot_assume_mapped = true
+
+    vim.keymap.set('i', '<M-l>', 'copilot#Accept("")', {
+      expr = true,
+      replace_keycodes = false,
+      desc = 'Copilot: Accept suggestion',
+    })
+    vim.keymap.set('i', '<M-.>', '<Plug>(copilot-next)', { remap = true, silent = true, desc = 'Copilot: Next suggestion' })
+    vim.keymap.set('i', '<M-,>', '<Plug>(copilot-previous)', { remap = true, silent = true, desc = 'Copilot: Previous suggestion' })
+    vim.keymap.set('i', '<M-;>', '<Plug>(copilot-dismiss)', { remap = true, silent = true, desc = 'Copilot: Dismiss suggestion' })
+  end,
+}
